414fab5f9c6b9d24b10c38f056ad69f9551d323d
Yohanan <yohananjr13@gmail.com>
2015-05-03 23:51:48 +0700

    add tab to home only

--- app/src/main/java/androidhive/info/materialdesign/activity/HomeFragment.java
+++ app/src/main/java/androidhive/info/materialdesign/activity/HomeFragment.java
@@ -4,14 +4,19 @@ package androidhive.info.materialdesign.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidhive.info.materialdesign.R;
+import androidhive.info.materialdesign.tabs.Tab1;
+import androidhive.info.materialdesign.tabs.Tab2;
 
 
 public class HomeFragment extends Fragment {
+
+    private FragmentTabHost mTabHost;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,6 +33,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // set up tab
+        mTabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("fragmentb").setIndicator("Fragment B"),
+                Tab1.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("fragmentc").setIndicator("Fragment C"),
+                Tab2.class, null);
 
         // Inflate the layout for this fragment
         return rootView;
--- app/src/main/java/androidhive/info/materialdesign/activity/MainActivity.java
+++ app/src/main/java/androidhive/info/materialdesign/activity/MainActivity.java
@@ -58,24 +58,24 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
 //        toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
         // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
-
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-        // Assigning the Sliding Tab Layout View
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
-            }
-        });
-
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
+//        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+//
+//        pager = (ViewPager) findViewById(R.id.pager);
+//        pager.setAdapter(adapter);
+//        // Assigning the Sliding Tab Layout View
+//        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+//        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+//
+//        // Setting Custom Color for the Scroll bar indicator of the Tab View
+//        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+//            @Override
+//            public int getIndicatorColor(int position) {
+//                return getResources().getColor(R.color.tabsScrollColor);
+//            }
+//        });
+//
+//        // Setting the ViewPager For the SlidingTabsLayout
+//        tabs.setViewPager(pager);
 
 
     }
